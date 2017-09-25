09f25ef86b250bacc5ada770c935f017222362ea
just-an-dev <just-an-dev@ggcorp.fr>
2017-09-25 16:11:15 +0200

    only start vanity thread if enabled

--- main.py
+++ main.py
@@ -43,9 +43,12 @@ if __name__ == "__main__":
                                                target=Bot.double_spend_check,
                                                args=(tx_queue, failover_time,))
 
-            thread_double_spend_check = Thread(name='vanitygen',
-                                               target=Bot.vanitygen,
-                                               args=(tx_queue, failover_time,))
+            if config.vanity_enabled:
+                thread_vanity = Thread(name='vanitygen',
+                                       target=Bot.vanitygen,
+                                       args=(tx_queue, failover_time,))
+                thread_vanity.setDaemon(True)
+                thread_vanity.start()
 
             thread_master.start()
             thread_pending_tip.start()
